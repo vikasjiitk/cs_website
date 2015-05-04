090de4c6ddcc8d927a367d550e8dc7e708c00788
Amlan Kar <amlan.asaa@gmail.com>
2015-05-04 06:19:39 +0530

    Added test csv file, and makejson.py

---
+++ family_tree/makejson.py
@@ -0,0 +1,47 @@
+import json
+import csv
+
+def dictmake(key):
+    dict_in = {}
+    dict_in["name"] = key
+    if key in sg:
+	sg.remove(key)
+	dict_in["children"] = []
+	for child in sgdict[key]:
+	    dict_in["children"].append(dictmake(child))
+    
+    return dict_in
+
+
+sgdict = {}
+##Make a dictionary with key as SG and value as an array of his/her bacchas/bacchis from csv file
+
+with open("sg.csv","rb") as sgfile:
+    reader = csv.reader(sgfile)
+    for row in reader:
+	key = row[0]+"-"+row[1]
+	value = row[2]+"-"+row[3]
+	if key not in sgdict.keys():
+	    sgdict[key] = []
+	sgdict[key].append(value)
+
+print sgdict
+sg = []
+sg = sgdict.keys() ##Will use this array to control the recursive function
+
+jsondict = {}
+jsondict["name"] = "all"
+jsondict["children"] = []
+
+with open("sg.csv","rb") as sgfile:
+    reader = csv.reader(sgfile)
+    for row in reader:
+	key = row[0]+"-"+row[1]
+	print key
+	if key in sg:
+	    print "SG Found"
+	    jsondict["children"].append(dictmake(key))
+
+with open("data.json","wb") as file:
+    json.dump(jsondict,file)
+
