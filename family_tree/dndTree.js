753507d5c67142d137b98ad205fc4f6132e829d0
Amlan Kar <amlan.asaa@gmail.com>
2015-05-20 18:20:08 +0530

    Changed spacing between names

--- family_tree/dndTree.js
+++ family_tree/dndTree.js
@@ -244,7 +244,7 @@ treeJSON = d3.json("data.json", function(error, treeData) {
             }
         };
         childCount(0, root);
-        var newHeight = d3.max(levelWidth) * 25; // 25 pixels per line  
+        var newHeight = d3.max(levelWidth) * 60; // 25 pixels per line  
         tree = tree.size([newHeight, viewerWidth]);
 
         // Compute the new tree layout.
